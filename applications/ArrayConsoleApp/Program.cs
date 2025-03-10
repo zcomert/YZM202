00b48cf928205f7929d9edff5b0e6e621c3b9968
drmsbgr <drmsbgr@gmail.com>
2025-03-10 11:45:20 +0300

    deneme aaa

---
+++ applications/ArrayConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using DataStructures.Array;
+
+var myArray = new Array<int>(5);
+myArray.Add(3);
+myArray.Add(5);
+myArray.Add(8);
+myArray.Add(6);
+myArray.Add(1);
+
+for (int i = 0; i < myArray.Length; i++)
+    Console.WriteLine(myArray[i]);
+
