1ee09d94eba09ff3b29809e9449c7a9622efb487
Ahsen Ece <231118014@samsun.edu.tr>
2025-04-11 12:16:13 +0300

    BaseConversionWithStack has been added.

--- problems/BaseConversionWithStack/Program.cs
+++ problems/BaseConversionWithStack/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Conversion(int number, int nBase)
+{
+    var conTable = "0123456789ABCDEF";
+    string result = "";
+    var stack = new DataStructures.Stack.ArrayStack<string>();
+    while (number > 0)
+    {
+        var over = number % nBase;
+        stack.Push(conTable[over].ToString());
+        number /= nBase;
+    }
+    while (stack.Count != 0)
+    {
+        result += stack.Pop();
+    }
+
+    return result;
+}
+
+Console.WriteLine(Conversion(156, 16));
+
+Console.ReadKey();
