0c969f4b6c4c439188590b9bcb59ecec60f99db3
drmsbgr <drmsbgr@gmail.com>
2025-04-13 22:20:58 +0300

    balanced brackets problem solved and tested

---
+++ tests/BalancedBracketTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace BalancedBracketTest;
+using Xunit;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("[{]", false)]
+    [InlineData("(())", true)]
+    [InlineData("{}]}", false)]
+    [InlineData("{({})}", true)]
+    public void Test1(string input, bool expected)
+    {
+        var result = BalancedParenthesesWithStack.BalancedBracketChecker.CheckBalanced(input);
+        Assert.Equal(expected, result);
+    }
+}
